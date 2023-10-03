544ae6ac5a9a7cf1fc33278895c618e602a01a4d
shrutanten <shrutanten.work@gmail.com>
2023-10-03 15:02:52 +0530

    Finish navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,36 +7,24 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import { useTheme } from "@mui/material";
 
 import logo from "../../assets/navbar/logo.svg";
 
 const pages = ["Home", "Converter", "About Us", "Contact Us"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
 	const theme = useTheme();
 	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
 	};
-	const handleOpenUserMenu = (event) => {
-		setAnchorElUser(event.currentTarget);
-	};
 
 	const handleCloseNavMenu = () => {
 		setAnchorElNav(null);
-	};
-
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
 	};
 
 	return (
@@ -57,7 +45,7 @@ function ResponsiveAppBar() {
 				}}
 			>
 				<Toolbar disableGutters>
-					<Box sx={{ height: 55 }}>
+					<Box sx={{ height: 55, display: { xs: "none", md: "flex" } }}>
 						<img src={logo} alt="logo" />
 					</Box>
 					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -96,65 +84,44 @@ function ResponsiveAppBar() {
 							))}
 						</Menu>
 					</Box>
-					<AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-					<Typography
-						variant="h5"
-						noWrap
-						component="a"
-						href="#app-bar-with-responsive-menu"
+					<Box
 						sx={{
-							mr: 2,
+							height: 40,
 							display: { xs: "flex", md: "none" },
 							flexGrow: 1,
-							fontFamily: "monospace",
-							fontWeight: 700,
-							letterSpacing: ".3rem",
-							color: "inherit",
-							textDecoration: "none",
 						}}
 					>
-						LOGO
-					</Typography>
-					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						<img src={logo} alt="logo" />
+					</Box>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: "none", md: "flex" },
+							gap: 1,
+							marginLeft: 1,
+						}}
+					>
 						{pages.map((page) => (
 							<Button
 								key={page}
 								onClick={handleCloseNavMenu}
-								sx={{ my: 2, color: "white", display: "block" }}
+								sx={{ my: 2, display: "block" }}
 							>
 								{page}
 							</Button>
 						))}
 					</Box>
 
-					<Box sx={{ flexGrow: 0 }}>
-						<Tooltip title="Open settings">
-							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-								<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-							</IconButton>
-						</Tooltip>
-						<Menu
-							sx={{ mt: "45px" }}
-							id="menu-appbar"
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: "top",
-								horizontal: "right",
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: "top",
-								horizontal: "right",
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}
+					<Box
+						sx={{ display: { md: "flex", xs: "block" }, flexGrow: 0, gap: 1 }}
+					>
+						<Button
+							variant="outlined"
+							sx={{ display: { xs: "none", md: "block" } }}
 						>
-							{settings.map((setting) => (
-								<MenuItem key={setting} onClick={handleCloseUserMenu}>
-									<Typography textAlign="center">{setting}</Typography>
-								</MenuItem>
-							))}
-						</Menu>
+							Log in
+						</Button>
+						<Button variant="contained">Sign up</Button>
 					</Box>
 				</Toolbar>
 			</Container>
