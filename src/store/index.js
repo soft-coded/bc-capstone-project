6945f5b3119c4e2d2895de54d12d9cf851018ae2
shrutanten <shrutanten.work@gmail.com>
2023-10-05 10:13:46 +0530

    Hook in authReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./slices/auth-slice";
+
 const store = configureStore({
-	reducer: {},
+	reducer: {
+		auth: authReducer,
+	},
 });
 
 export default store;
