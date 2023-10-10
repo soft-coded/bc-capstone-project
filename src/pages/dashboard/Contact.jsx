9984795ed22a85185ce3af74742722c937ec8346
1192716 <malayraj99@gmail.com>
2023-10-10 12:59:36 +0530

    Added Validations to contact form

--- src/pages/dashboard/Contact.jsx
+++ src/pages/dashboard/Contact.jsx
@@ -9,8 +9,60 @@ import {
 	// MenuItem,
 	FormControl,
 } from "@mui/material";
+import React, { useState } from "react";
 
 function ContactForm() {
+	const [contactData, setContactData] = useState({
+		name: "",
+		// lastName: "",
+		email: "",
+		description: "",
+	});
+	const [contactErrors, setContactErrors] = useState({
+		name: "",
+		// lastName: "",
+		email: "",
+		description: "",
+	});
+	const validateForm = () => {
+		let isValid = true;
+		const newErrors = { ...contactErrors };
+
+		// Validation logic for Email
+		if (
+			!contactData.email.match(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)
+		) {
+			newErrors.email = "Invalid Email";
+			isValid = false;
+		} else {
+			newErrors.email = "";
+		}
+
+		// Validation logic for Descrition
+		if (contactData.description.length > 250) {
+			newErrors.description = "Description must be at less than 250 characters";
+			isValid = false;
+		} else {
+			newErrors.description = "";
+		}
+
+		setContactErrors(newErrors);
+		return isValid;
+	};
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setContactData({
+			...contactData,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!validateForm()) return;
+	};
+
 	return (
 		<Box
 			width="100%"
@@ -18,7 +70,7 @@ function ContactForm() {
 			padding={3}
 			borderRadius="30px"
 		>
-			<form style={{ width: "100%" }}>
+			<form style={{ width: "100%" }} onSubmit={handleSubmit}>
 				<Grid
 					container
 					display="flex"
@@ -41,14 +93,15 @@ function ContactForm() {
 									Name
 								</FormLabel>
 								<TextField
-									id="name"
-									placeholder="Suresh Agarwal"
+									// id="name"
+									placeholder="Your Name"
 									variant="outlined"
-									name="amount"
-									// value={formData.password}
-									// onChange={handleChange}
+									name="name"
+									value={contactData.name}
+									onChange={handleInputChange}
+									required
 								/>
-								{/* <div style={{ color: "red" }}>{formErrors.password}</div> */}
+								<div style={{ color: "red" }}>{contactErrors.name}</div>
 							</FormControl>
 						</Grid>
 						<Grid item width="48%">
@@ -57,15 +110,16 @@ function ContactForm() {
 									Email
 								</FormLabel>
 								<TextField
-									id="email"
+									// id="email"
 									placeholder="you@example.com"
 									variant="outlined"
 									name="email"
 									type="email"
-									// value={formData.password}
-									// onChange={handleChange}
+									value={contactData.email}
+									onChange={handleInputChange}
+									required
 								/>
-								{/* <div style={{ color: "red" }}>{formErrors.password}</div> */}
+								<div style={{ color: "red" }}>{contactErrors.email}</div>
 							</FormControl>
 						</Grid>
 					</Grid>
@@ -75,18 +129,19 @@ function ContactForm() {
 								How can we help you?
 							</FormLabel>
 							<TextField
-								id="description"
+								// id="description"
 								placeholder="Describe your problem in 250 words or less."
 								variant="outlined"
-								name="email"
-								type="email"
+								name="description"
+								// type="email"
 								rows={7}
 								multiline
 								sx={{ borderRadius: "0px" }}
-								// value={formData.password}
-								// onChange={handleChange}
+								value={contactData.description}
+								onChange={handleInputChange}
+								required
 							/>
-							{/* <div style={{ color: "red" }}>{formErrors.password}</div> */}
+							<div style={{ color: "red" }}>{contactErrors.description}</div>
 						</FormControl>
 					</Grid>
 					<Grid item width="100%" display="flex" justifyContent="center">
